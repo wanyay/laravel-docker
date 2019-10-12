60cdeeaeb8086d20105c4c90dbab9e218b5f196b
Wai Yan <seasoft.waiyan@gmail.com>
2019-10-12 15:46:12 +0630

    add some php extensions

--- shared/install_php_extensions.sh
+++ shared/install_php_extensions.sh
@@ -2,7 +2,7 @@
 
 # add wget
 apt-get update -yqq && apt-get -f install -yyq wget libwebp-dev libjpeg62-turbo-dev libpng-dev libxpm-dev \
-    libfreetype6-dev
+    libfreetype6-dev libcurl4-openssl-dev pkg-config libssl-dev
 
 # download helper script
 # @see https://github.com/mlocati/docker-php-extension-installer/
@@ -13,8 +13,12 @@ docker-php-ext-configure gd --with-gd --with-webp-dir --with-jpeg-dir \
     --with-png-dir --with-zlib-dir --with-xpm-dir --with-freetype-dir \
     --enable-gd-native-ttf
 
+git clone https://github.com/phpredis/phpredis.git /usr/src/php/ext/redis
+
 # install extensions
-chmod uga+x /usr/local/bin/install-php-extensions && sync && install-php-extensions \
+chmod uga+x /usr/local/bin/install-php-extensions && sync && install-php-extensions redis \
+	mongodb \
+    bcmath \
 	pdo_mysql\
     mysqli \
     gd \
